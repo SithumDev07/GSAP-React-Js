1f865a66e7ebf0d697a160311469955e3f825560
Sithum Basnayaka <sithum.dashantha@gmail.com>
2021-09-29 01:49:59 +0530

    useRef preventing glitch when loading

--- src/App.js
+++ src/App.js
@@ -1,30 +1,61 @@
 import React, {useRef, useEffect} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-// import {Power3} from 'gsap'
+import {gsap,Power3} from 'gsap'
 
 function App() {
-  let logoItem = useRef();
+  let app = useRef(null);
+  let circle = useRef(null)
+  let circleRed = useRef(null)
+  let circleBlue = useRef(null)
 
   useEffect(() => {
+    gsap.to(
+      app,
+      {
+        duration: 0,
+        css: {visibility: 'visible'}
+      }
+    );
+    gsap.from(
+      circle,
+      {
+        duration: .9,
+        opacity: 0,
+        x: 40,
+        ease: Power3.easeOut,
+      }
+      );
+      gsap.from(
+        circleRed,
+        {
+          duration: .9,
+          opacity: 0,
+          x: 40,
+          ease: Power3.easeOut,
+          delay: .2
+      }
+    );
+    gsap.from(
+      circleBlue,
+      {
+        duration: .9,
+        opacity: 0,
+        x: 40,
+        ease: Power3.easeOut,
+        delay: .4
+      }
+    );
   }, []);
 
   return (
-    <div className="App">
+    <div className="App" ref={el => {app = el}}>
       <header className="App-header">
-        <img ref={element => {logoItem = element}} src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <div className="circle-container">
+          <div className="circle" ref={el => {circle = el}}></div>
+          <div className="circle red" ref={el => {circleRed = el}}></div>
+          <div className="circle blue" ref={el => {circleBlue = el}}></div>
+       </div>
       </header>
     </div>
   );
